03e6491f9e4f4e3f30a7a6e4aa16fc1429feeb20
ayham <ayhamgm81@gmail.com>
2023-01-21 14:07:07 +0200

    4th commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,14 @@ const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = ["/", "/public/css/index.css"];
+const PRECACHE_URLS = [
+  "/",
+  "/auth",
+  "/new-calendar",
+  "/public/css/index.css",
+  "/public/css/SuperCalendar.css",
+  "/public/manifest.json",
+];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener("install", (event) => {
